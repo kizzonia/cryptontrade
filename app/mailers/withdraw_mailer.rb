aa1135d0c4fa24243732027d188408ac0f0bd9d4
kizzonia <chibuikefortune@hotmail.com>
2018-10-14 22:59:44 +0100

    start

--- app/mailers/withdraw_mailer.rb
+++ app/mailers/withdraw_mailer.rb
@@ -1,8 +1,8 @@
 class WithdrawMailer < ApplicationMailer
-  default :from => "support@xpressbittrade.com"
+  default :from => "support@cryptontrades.com"
   def withdraw_email(user, withdraw)
     @user = user
     @withdraw = withdraw
-     mail(to: "support@xpressbittrade.com", subject: 'withdraw Request')
+     mail(to: "support@cryptontrades.com", subject: 'withdraw Request')
   end
 end
