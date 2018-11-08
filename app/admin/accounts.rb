1c166ec45920355881cfd88b3acf9c294e63e59c
kizzonia <chibuikefortune@hotmail.com>
2018-11-08 12:03:11 +0100

    near done

--- app/admin/accounts.rb
+++ app/admin/accounts.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Account do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :balance, :user_id, :address, :slug
+  permit_params :balance, :user_id, :address, :slug, :capital, :roi, :guranted, :gross_pl, :net_pl
 
   form do |f|
 
